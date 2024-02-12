37421d084e186ab4ad6c3909700ef0a7bc53be5c
Petr Stojanov <petrstojanov@gmail.com>
2024-02-12 21:26:59 +0100

    Tests

--- validator_test.go
+++ validator_test.go
@@ -11,7 +11,6 @@ import (
 
 	"go.dataddo.com/pgq/internal/pg"
 	"go.dataddo.com/pgq/internal/require"
-	"go.dataddo.com/pgq/x/schema"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
@@ -23,10 +22,10 @@ func TestValidator_ValidateFieldsCorrectSchema(t *testing.T) {
 	db := openDB(t)
 	queueName := fmt.Sprintf("TestQueue_%s", generateRandomString(10))
 
-	defer db.ExecContext(ctx, schema.GenerateDropTableQuery(queueName))
+	defer db.ExecContext(ctx, generateDropTableQuery(queueName))
 
 	// Create the new queue
-	_, err := db.ExecContext(ctx, schema.GenerateCreateTableQuery(queueName))
+	_, err := db.ExecContext(ctx, generateCreateTableQuery(queueName))
 	require.NoError(t, err)
 
 	// --- (2) ----
@@ -58,38 +57,13 @@ func TestValidator_ValidateFieldsCorrectSchemaPartitionedTable(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func generateCreateTablePartitionedQuery(queueName string) string {
-	quotedTableName := pg.QuoteIdentifier(queueName)
-	return fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %[1]s
-	(
-		id             UUID        DEFAULT gen_random_uuid() NOT NULL,
-		created_at     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
-		started_at     TIMESTAMPTZ                           NULL,
-		locked_until   TIMESTAMPTZ                           NULL,
-		processed_at   TIMESTAMPTZ                           NULL,
-		consumed_count INTEGER     DEFAULT 0                 NOT NULL,
-		error_detail   TEXT                                  NULL,
-		payload        JSONB                                 NOT NULL,
-		metadata       JSONB                                 NOT NULL
-	) PARTITION BY RANGE (created_at);
-	CREATE TABLE "%[2]s_y2024m02" PARTITION OF %[1]s FOR VALUES FROM ('2024-01-01') TO ('2024-02-01');
-	CREATE INDEX IF NOT EXISTS "%[2]s_created_at_idx" ON %[1]s (created_at);
-	CREATE INDEX IF NOT EXISTS "%[2]s_processed_at_null_idx" ON %[1]s (processed_at) WHERE (processed_at IS NULL);
-	`, quotedTableName, quotedTableName[1:len(quotedTableName)-1])
-}
-
-func generateDropTableQuery(queueName string) string {
-	quotedTableName := pg.QuoteIdentifier(queueName)
-	return `DROP TABLE IF EXISTS ` + quotedTableName
-}
-
 func TestValidator_ValidateFieldsIncorrectSchema(t *testing.T) {
 	// --- (1) ----
 	// Arrange
 	ctx := context.Background()
 	db := openDB(t)
 	queueName := fmt.Sprintf("TestQueue_%s", generateRandomString(10))
-	defer db.ExecContext(ctx, schema.GenerateDropTableQuery(queueName))
+	defer db.ExecContext(ctx, generateDropTableQuery(queueName))
 
 	// Create the new incorrect queue
 	_, err := db.ExecContext(ctx, generateInvalidQueueQuery(queueName))
@@ -110,10 +84,10 @@ func TestValidator_ValidateIndexesCorrectSchema(t *testing.T) {
 	db := openDB(t)
 	queueName := fmt.Sprintf("TestQueue_%s", generateRandomString(10))
 
-	defer db.ExecContext(ctx, schema.GenerateDropTableQuery(queueName))
+	defer db.ExecContext(ctx, generateDropTableQuery(queueName))
 
 	// Create the new queue
-	_, err := db.ExecContext(ctx, schema.GenerateCreateTableQuery(queueName))
+	_, err := db.ExecContext(ctx, generateCreateTableQuery(queueName))
 	require.NoError(t, err)
 
 	// --- (2) ----
@@ -131,10 +105,10 @@ func TestValidator_ValidateIndexesCorrectSchema_CompositeIndexes(t *testing.T) {
 	db := openDB(t)
 	queueName := fmt.Sprintf("TestQueue_%s", generateRandomString(10))
 
-	defer db.ExecContext(ctx, schema.GenerateDropTableQuery(queueName))
+	defer db.ExecContext(ctx, generateDropTableQuery(queueName))
 
 	// Create the new queue
-	_, err := db.ExecContext(ctx, schema.GenerateCreateTableQueryCompositeIndex(queueName))
+	_, err := db.ExecContext(ctx, generateCreateTableQueryCompositeIndex(queueName))
 	require.NoError(t, err)
 
 	// --- (2) ----
@@ -151,7 +125,7 @@ func TestValidator_ValidateIndexesIncorrectSchema(t *testing.T) {
 	ctx := context.Background()
 	db := openDB(t)
 	queueName := fmt.Sprintf("TestQueue_%s", generateRandomString(10))
-	defer db.ExecContext(ctx, schema.GenerateDropTableQuery(queueName))
+	defer db.ExecContext(ctx, generateDropTableQuery(queueName))
 
 	// Create the new incorrect queue
 	_, err := db.ExecContext(ctx, generateInvalidQueueQuery(queueName))
@@ -215,4 +189,67 @@ func generateInvalidQueueQuery(queueName string) string {
 		name		   TEXT 								 NULL
 	);
 	`, quotedTableName, quotedTableName[1:len(quotedTableName)-1])
+}
+
+func generateCreateTableQuery(queueName string) string {
+	quotedTableName := pg.QuoteIdentifier(queueName)
+	return fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %[1]s
+	(
+		id             UUID        DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+		created_at     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		started_at     TIMESTAMPTZ                           NULL,
+		locked_until   TIMESTAMPTZ                           NULL,
+		processed_at   TIMESTAMPTZ                           NULL,
+		consumed_count INTEGER     DEFAULT 0                 NOT NULL,
+		error_detail   TEXT                                  NULL,
+		payload        JSONB                                 NOT NULL,
+		metadata       JSONB                                 NOT NULL
+	);
+	CREATE INDEX IF NOT EXISTS "%[2]s_created_at_idx" ON %[1]s (created_at);
+	CREATE INDEX IF NOT EXISTS "%[2]s_processed_at_null_idx" ON %[1]s (processed_at) WHERE (processed_at IS NULL);
+	`, quotedTableName, quotedTableName[1:len(quotedTableName)-1])
+}
+
+func generateCreateTableQueryCompositeIndex(queueName string) string {
+	quotedTableName := pg.QuoteIdentifier(queueName)
+	return fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %[1]s
+	(
+		id             UUID        DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+		created_at     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		started_at     TIMESTAMPTZ                           NULL,
+		locked_until   TIMESTAMPTZ                           NULL,
+		processed_at   TIMESTAMPTZ                           NULL,
+		consumed_count INTEGER     DEFAULT 0                 NOT NULL,
+		error_detail   TEXT                                  NULL,
+		payload        JSONB                                 NOT NULL,
+		metadata       JSONB                                 NOT NULL
+	);
+	CREATE INDEX IF NOT EXISTS "%[2]s_created_at_idx" ON %[1]s (created_at);
+	CREATE INDEX IF NOT EXISTS "%[2]s_processed_at_null_idx" ON %[1]s (consumed_count, processed_at) WHERE (processed_at IS NULL);
+	`, quotedTableName, quotedTableName[1:len(quotedTableName)-1])
+}
+
+func generateCreateTablePartitionedQuery(queueName string) string {
+	quotedTableName := pg.QuoteIdentifier(queueName)
+	return fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %[1]s
+	(
+		id             UUID        DEFAULT gen_random_uuid() NOT NULL,
+		created_at     TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		started_at     TIMESTAMPTZ                           NULL,
+		locked_until   TIMESTAMPTZ                           NULL,
+		processed_at   TIMESTAMPTZ                           NULL,
+		consumed_count INTEGER     DEFAULT 0                 NOT NULL,
+		error_detail   TEXT                                  NULL,
+		payload        JSONB                                 NOT NULL,
+		metadata       JSONB                                 NOT NULL
+	) PARTITION BY RANGE (created_at);
+	CREATE TABLE "%[2]s_y2024m02" PARTITION OF %[1]s FOR VALUES FROM ('2024-01-01') TO ('2024-02-01');
+	CREATE INDEX IF NOT EXISTS "%[2]s_created_at_idx" ON %[1]s (created_at);
+	CREATE INDEX IF NOT EXISTS "%[2]s_processed_at_null_idx" ON %[1]s (processed_at) WHERE (processed_at IS NULL);
+	`, quotedTableName, quotedTableName[1:len(quotedTableName)-1])
+}
+
+func generateDropTableQuery(queueName string) string {
+	quotedTableName := pg.QuoteIdentifier(queueName)
+	return `DROP TABLE IF EXISTS ` + quotedTableName
 }
